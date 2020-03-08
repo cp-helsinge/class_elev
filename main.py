1825b0f9d523c6d97dbbeb29502a3f86fd5efe72
SplatManDK <github4js@conceptfactory.dk>
2020-03-08 14:10:10 +0100

    Første fil

---
+++ main.py
@@ -0,0 +1,9 @@
+# Dette er hovedfilen som instuktørerne kan proppe indhold i
+# Elever skal holde sig uden for denne fil
+
+class Elev
+    fornavn=""
+    efternavn=""
+    arme=2
+    ben=2
+    uartig="Nej"
