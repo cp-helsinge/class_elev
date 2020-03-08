edbe2dedf532aab412e515e9d5bfddd5d84c4428
SplatManDK <github4js@conceptfactory.dk>
2020-03-08 14:15:31 +0100

    Opdateret klasse eksempel

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # Dette er hovedfilen som instuktørerne kan proppe indhold i
 # Elever skal holde sig uden for denne fil
 
-class Elev
+class Menneske
     fornavn=""
     efternavn=""
     arme=2
     ben=2
+
+class DerivedClass(Menneske): Elev
     uartig="Nej"
+
+class DerivedClass(Menneske): Underviser
+    streng="Ja"
+ 
