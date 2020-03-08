bb073375aface7e84b908ea93d9d273a8393b094
SplatManDK <github4js@conceptfactory.dk>
2020-03-08 14:30:26 +0100

    Simons rettelser igen

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ class Menneske:
         efternavn=""
         arme=2
         ben=2
+
     def antalArme(self):
         return self.arme
-    def antalBen(self): ben
+
+    def antalBen(self):
         return self.ben
 
 class DerivedClass(Menneske): Elev
     ''' Dette er vores Elev klasse som nedarver fra Menneske klassen '''
-        def __init__ (self)
+    def __init__ (self):
         uartig="Nej"
+
     def erUartig(self):
         return self.arme
 
 
 class DerivedClass(Menneske): Underviser
     ''' Dette er vores Underviser klasse som nedarver fra Menneske klassen '''
-    def __init__ (self)
+
+    def __init__ (self):
         streng="Ja"
+
     def erStreng(self):
         return self.uartig
 
